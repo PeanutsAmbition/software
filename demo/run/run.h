dca0b78b6f5ceb696738ff4c227dcc91d7744049
Yuta Inoue <umaibow1999@gmail.com>
2014-10-14 23:24:24 +0900

    ライブラリの説明

--- demo/run/run.h
+++ demo/run/run.h
@@ -1,28 +1,28 @@
 #include "QEI.h"
 #define PI 3.14159265359
-#define wheelGain 0
-#define speed 0.7
+#define wheelGain 0	　		//減速速度
+#define speed 0.7			//最高速度
 
 
 class run{
 	
 	public:
-		run();
-		void straight(float);
-		void turn(float);
-		void WheelReset(void);
+		run();			//コンストラクタ
+		void straight(float);	//直進(引数は距離(mm))
+		void turn(float);	//旋回(引数は角度(deg))
+		void WheelReset(void);	//エンコーダの値のリセット
 		
-		float RightLength;
-		float LeftLength;
-		float Theta[2];
-		float dTheta;
-		float Length[2];
-		float dLength;
+		float RightLength;	//右の車輪の進行距離
+		float LeftLength;	//左の車輪の進行距離
+		float Theta[2];		//旋回した角度
+		float dTheta;		//角速度
+		float Length[2];	//進行距離
+		float dLength;		//進行速度
 	
 	private:
-		float wheel;
+		float wheel;		//車輪の間隔
 		
-		void getLength(void);
+		void getLength(void);	//エンコーダの処理
 		
 };
 		
