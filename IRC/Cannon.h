5c930c565e5b8858322f2ed5ecd38316df318657
Yu Kato <9a10man@gmail.com>
2014-12-02 17:17:33 +0900

    mainの追加
    mainを追加しました

---
+++ IRC/Cannon.h
@@ -0,0 +1,29 @@
+#ifndef _CANNON_H
+#define _CANNON_H
+#include "mbed.h"
+enum
+{
+	TIMER_LIMIT =100
+};
+
+
+public class Cannon
+{
+public:
+	Cannon(void);
+	//~Cannon(void);
+
+	void Fire(void);
+
+	int t;	//ƒ‚[ƒ^‚ğ“®‚©‚µ‚Ä‚¢‚éŠÔ
+      
+};
+
+
+
+
+
+
+
+
+#endif	//_CANNON_H
