e6afdecc5d71248cb28c07ded8c31e84902f2d35
umaibow <umaibow1999@gmail.com>
2014-12-03 16:53:42 +0900

    修正

--- demo/sensor/sensor.h
+++ demo/sensor/sensor.h
@@ -3,9 +3,9 @@
 
 
 AnalogIn	psd1(p17);
-AanlogIn	psd2(p18);
+AnalogIn	psd2(p18);
 AnalogIn	psd3(p19);
-Analogin	psd4(p20);
+AnalogIn	psd4(p20);
 
 ColorSensor ColorSensor(p11, p12, p13, p14, 30);
 
